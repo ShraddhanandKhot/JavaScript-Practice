db251c2ba0d843a49d02c5f1c040cc1491bce416
ShraddhanandKhot <shraddhanand2836@gmail.com>
2025-11-05 12:05:39 +0530

    const and let differences

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 // Keywords var, let , const 
  let a =20;
- console.log(a)
+ console.log(a) // o/p 20
+ a=23
+ console.log(a) // o/p 23
+
+ const b =30;
+ console.log(b) // o/p 30
+
+ // b =32;   this will throw an erron -> TypeError: Assignment to constant variable.
